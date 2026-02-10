8d79e7c3e4d5ce332b82a66e84c5393b852356ab
bakethecake57-svg <bakethecake57@gmail.com>
2026-02-09 21:05:51 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://music.apple.com/us/album/cari%C3%B1o/1436703562?i=1436703569", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 0.8                        // Volume level (0.0 to 1.0)
     }
 };
 
